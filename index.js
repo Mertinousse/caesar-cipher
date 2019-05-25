4b209d23b6ec8bc3dc8f7a738d4d04de9c30542c
Mertinousse <mertinousse@gmail.com>
2019-05-25 19:18:15 +0200

    Encode and decode

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 (() => {
   const application = Stimulus.Application.start()
 
-  application.register('offset', class extends Stimulus.Controller {
-    static targets = [ 'key' ]
+  application.register('cipher', class extends Stimulus.Controller {
+    static targets = [ 'key', 'text' ]
 
-    get() {
+    get alphabet() { return 'abcdefghijklmnopqrstuvwxyz'.split('') }
+    get key() { return this.keyTarget.value }
+    get text() { return this.textTarget.value }
+
+    offset() {
       if (!this.key) { return }
       axios({
         method: 'post',
@@ -13,14 +17,53 @@
           key: this.key
         }
       }).then(data => {
-        this.data.set('value', data.data.offset)
+        this.data.set('offset', data.data.offset)
       }).catch(err => {
         console.log(err)
       })
     }
 
-    get key() {
-      return this.keyTarget.value
+    encode() {
+      const offset = parseInt(this.data.get('offset'))
+      this.code(offset)
+    }
+
+    decode() {
+      const offset = parseInt(this.data.get('offset'))
+      this.code(this.alphabet.length - offset)
+    }
+
+    code(offset) {
+      if (!this.text) { return }
+      const text = this.text
+      let coded = ''
+      
+      for (var i = 0; i < text.length; i++) {
+        const char = text.charAt(i)
+        let shifted
+        if (char == ' ') {
+          shifted = char
+        } else if (this.check(char)) {
+          shifted = this.shift(char, offset)
+        } else { return }
+        coded = coded.concat(shifted)
+      }
+      alert(coded)
+    }
+
+    shift(value, offset) {
+      const alphabet = this.alphabet
+      const isCapital = !alphabet.includes(value)
+      const index = (alphabet.indexOf(value.toLowerCase()) + offset) % alphabet.length
+      let shifted = alphabet[index]
+      if (isCapital) { shifted = shifted.toUpperCase() }
+      return shifted
+    }
+
+    check(char) {
+      if (this.alphabet.includes(char.toLowerCase())) { return true }
+      alert('Error: invalid text')
+      return false     
     }
   })
 })()
